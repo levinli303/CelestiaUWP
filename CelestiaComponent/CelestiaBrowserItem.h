3524abaa27667d501ec0e031c8c5f41b8282d96f
Levin Li <lilinfeng303@outlook.com>
2022-02-09 20:50:15 +0800

    temp

--- CelestiaComponent/CelestiaBrowserItem.h
+++ CelestiaComponent/CelestiaBrowserItem.h
@@ -21,7 +21,7 @@ namespace winrt::CelestiaComponent::implementation
 {
     struct CelestiaBrowserItem : CelestiaBrowserItemT<CelestiaBrowserItem>
     {
-        CelestiaBrowserItem(hstring name, array_view<CelestiaComponent::CelestiaBrowserItem const> children);
+        CelestiaBrowserItem(hstring name, array_view<CelestiaComponent::CelestiaBrowserItem const> children, bool sorted);
         CelestiaBrowserItem(hstring name, CelestiaComponent::CelestiaAstroObject const& obj, CelestiaComponent::CelestiaBrowserItemChildrenProvider const& provider);
 
         CelestiaComponent::CelestiaBrowserItemChildrenProvider Provider();
@@ -36,6 +36,7 @@ namespace winrt::CelestiaComponent::implementation
         hstring name;
         std::vector<CelestiaComponent::CelestiaBrowserItem> children;
         bool areChildrenLoaded;
+        bool sorted;
     };
 }
 
